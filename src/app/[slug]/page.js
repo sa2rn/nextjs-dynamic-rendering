695f4b6406fd4ed2f968bb4e24c85e795e106f62
Olexandr Stepanenko <sa2rn.set@gmail.com>
2024-04-02 13:07:04 +0300

    Cache requests

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,22 +1,23 @@
-import { notFound } from 'next/navigation'
+import { cache } from 'react';
+import { notFound } from 'next/navigation';
 import { getPageBySlug } from '@/lib/db';
 import renderer from '@/lib/renderer';
 
-async function fetchPage(slug) {
+const getPage = cache(async (slug) => {
   const page = await getPageBySlug(slug);
   if (!page) {
     notFound();
   }
   return page;
-}
+});
 
 export default async function Page({ params }) {
-  const page = await fetchPage(params.slug);
+  const page = await getPage(params.slug);
   return renderer(page.blocks);
 }
 
 export async function generateMetadata({ params }) {
-  const page = await fetchPage(params.slug);
+  const page = await getPage(params.slug);
   return {
     title: page.title,
     description: page.description,
